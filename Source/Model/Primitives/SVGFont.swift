56df0bcdec15eed6fbee1c943b03b3783647e7bb
Cristian-A <cristianantonuccio@icloud.com>
2023-10-21 11:34:21 +0200

    Added font-weight support

--- Source/Model/Primitives/SVGFont.swift
+++ Source/Model/Primitives/SVGFont.swift
@@ -13,12 +13,34 @@ public class SVGFont: SerializableBlock {
     }
 
     public func toSwiftUI() -> Font {
-        return Font.custom(name, size: size)//.weight(fontWeight)
+		return Font.custom(name, size: size).weight(fontWeight(from: weight))
     }
 
     func serialize(_ serializer: Serializer) {
         serializer.add("name", name, "Serif").add("size", size, 16).add("weight", weight, "normal")
     }
+	
+	private func fontWeight(from: String) -> Font.Weight {
+		// TODO: lighter and bolder are context-based, this is temporary.
+		switch weight {
+		case "lighter": return .light
+		case  "normal": return .regular
+		case    "bold": return .bold
+		case  "bolder": return .black
+		case "100": return .ultraLight
+		case "200": return .thin
+		case "300": return .light
+		case "400": return .regular
+		case "500": return .medium
+		case "600": return .semibold
+		case "700": return .bold
+		case "800": return .heavy
+		case "900": return .black
+		default:    return .regular
+		}
+	}
+	
+	
 }
 
 
